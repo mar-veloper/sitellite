a6ebf235c49c241db91eead037a2251dbb4cf58d
mar-veloper <42553243+mar-veloper@users.noreply.github.com>
2020-05-07 11:34:57 +0200

    added some files

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import { Provider } from "react-redux";
-import { Switch, Route } from "react-router-dom";
+import { Switch, Route, Redirect } from "react-router-dom";
 
 import "./styles/App.css";
 import configureStore from "./store/configureStore";
 import Satellites from "./components/Satellites";
 import SpecificSatellite from "./components/SpecificSatellite";
+import Home from "./components/Home";
+import PageNotFound from "./components/PageNotFound";
 
 const store = configureStore();
 
@@ -13,8 +15,12 @@ function App() {
   return (
     <Provider store={store}>
       <Switch>
+        <Route path="/loading" component={Satellites} />
+        <Route exact path="/" component={Home} />
         <Route exact path="/satellites" component={Satellites} />
-        <Route exact path="/satellites/:id" component={SpecificSatellite} />
+        <Route path="/satellites/:id" component={SpecificSatellite} />
+        <Route path="/page-not-found" component={PageNotFound} />
+        <Redirect to="/page-not-found" />
       </Switch>
     </Provider>
   );
